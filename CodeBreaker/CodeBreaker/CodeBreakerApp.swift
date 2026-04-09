6eb0c97bc8a00cb87f3572d24d78876b2caa2f82
YiHui020 <2489263425@qq.com>
2026-04-10 00:19:24 +0800

    init

---
+++ CodeBreaker/CodeBreaker/CodeBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerApp.swift
+//  CodeBreaker
+//
+//  Created by YiHui020 on 2026/4/10.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
